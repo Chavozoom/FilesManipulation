3a9bc06d8e489f22f4807ba6ef132c493b7f3292
Chavozoom <isaquechaves10500@gmail.com>
2023-01-29 22:11:50 -0300

    Projeto Final

---
+++ FilesManipulation/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace FilesManipulation.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int Quantity { get; set; }
+        public Product()
+        {
+        }
+        public Product(string name, double value, int quantity)
+        {
+            Name = name;
+            Value = value;
+            Quantity = quantity;
+        }
+        public double Total()
+        {
+            return Value* Quantity;
+        }
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Total().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
